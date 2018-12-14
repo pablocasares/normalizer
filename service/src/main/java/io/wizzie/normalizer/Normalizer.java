9caa81b315822af9d07bfe8d0a0aede5745bc39d
Pablo Casares <lito92vforce@gmail.com>
2018-12-14 12:09:25 +0100

    Add logo print to normalizer (#45)

--- service/src/main/java/io/wizzie/normalizer/Normalizer.java
+++ service/src/main/java/io/wizzie/normalizer/Normalizer.java
@@ -2,6 +2,7 @@ package io.wizzie.normalizer;
 
 import io.wizzie.bootstrapper.builder.Config;
 import io.wizzie.normalizer.builder.Builder;
+import io.wizzie.normalizer.logo.LogoPrinter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,6 +10,7 @@ public class Normalizer {
     private static final Logger log = LoggerFactory.getLogger(Normalizer.class);
 
     public static void main(String[] args) throws Exception {
+        LogoPrinter.PrintLogo();
         if (args.length == 1) {
             log.info("Starting Normalizer engine.");
             Config config = new Config(args[0]);
